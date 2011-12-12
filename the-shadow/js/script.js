682e9eccac2753b152bec692ff435b02d0ec4fe1
Nathan Stilwell <nstilwell@gilt.com>
2011-12-11 19:20:24 -0500

    Text Shadow maker tool (initial commit)

---
+++ the-shadow/js/script.js
@@ -0,0 +1,38 @@
+/* Author: those jerks
+
+*/
+
+
+(function(global, doc, $, undefined){
+    
+  $(function onReady () {
+  
+     // Bind some events
+
+     //depth
+    $("#depth").bind("change", function(e) {
+       var val = $(this).val();
+       
+
+    });
+    // //color
+
+     //direction
+
+     //font
+
+  });
+
+})(this, this.document, this.jQuery);
+
+
+
+
+
+
+
+
+
+
+
+
