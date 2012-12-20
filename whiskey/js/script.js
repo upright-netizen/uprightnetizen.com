fbb0666e6e84a9287397fc9629618cf2f77c11a0
Nathan Stilwell <nstilwell@gilt.com>
2012-12-20 17:23:25 -0500

    whiskey class

---
+++ whiskey/js/script.js
@@ -0,0 +1,8 @@
+(function(global, $, undefined){
+    $(function(){
+      $('#presentation').fathom({
+        displayMode : 'multi',
+        margin : 5000
+      });
+    });
+}(this, this.jQuery));
