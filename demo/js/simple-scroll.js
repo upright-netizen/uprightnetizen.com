863fe2c1c396809646853597543926a3b8f75360
Nathan Stilwell <nathanstilwell@gmail.com>
2013-02-22 20:41:22 -0500

    demo

--- demo/js/simple-scroll.js
+++ demo/js/simple-scroll.js
@@ -2,18 +2,34 @@
 
   $(function () {
 
+    var nav = $('#nav-bar');
+    var body = $('html,body');
+    var win = $(window);
+
     $('body').on('click', '.scroll-to', function (e) {
       var self = $(this);
       var target = self.attr('href');
       var targetScrollTop = $(target).position().top;
+      var navItem = target.replace('#', 'nav-');
 
       e.preventDefault();
 
+      nav.find('.here').removeClass('here');
+      $('#' + navItem).addClass('here');
 
-      $("html,body").animate({
+      body.animate({
         'scrollTop' : targetScrollTop
       }, 1000);
     });
+
+    setTimeout(function () {
+      if (win.scrollTop() > 0) {
+        body.animate({
+          'scrollTop' : 0
+        }, 500);
+        console.log('scroll up');
+      }
+    }, 500);
 
   });
 
