bddffd2fbf694183f1412542dfba1a15ce4d72ae
Nathan Stilwell <nathanstilwell@gmail.com>
2014-07-20 22:23:57 -0400

    adding a background color choice

--- rebrand/app/app.js
+++ rebrand/app/app.js
@@ -1,38 +1,42 @@
 /*jshint esnext: true, laxcomma: true, eqeqeq: true, bitwise: true, curly: true, latedef: true, strict: true, plusplus: true*/
 /*global define: true */
 define([
-], function () {
+  'jquery'
+], function ($) {
   'use strict';
 
-/*
-      ,ggggggggggg,
-     dP"""88""""""Y8,                  I8
-     Yb,  88      `8b                  I8
-      `"  88      ,8P               88888888
-          88aaaad8P"                   I8
-          88""""",gggg,gg   ,gggggg,   I8   gg     gg
-          88    dP"  "Y8I   dP""""8I   I8   I8     8I
-          88   i8'    ,8I  ,8'    8I  ,I8,  I8,   ,8I
-          88  ,d8,   ,d8b,,dP     Y8,,d88b,,d8b, ,d8I
-          88  P"Y8888P"`Y88P      `Y88P""Y8P""Y88P"888
-                                                 ,d8I'
-                                               ,dP'8I
-                                              ,8"  8I
-                                              I8   8I
-                                              `8, ,8I
-                                               `Y8P"
 
-        ,ggggggggggggggg
-       dP""""""88"""""""
-       Yb,_    88
-        `""    88   gg
-               88   ""
-               88   gg    ,ggg,,ggg,,ggg,    ,ggg,
-               88   88   ,8" "8P" "8P" "8,  i8" "8i
-         gg,   88   88   I8   8I   8I   8I  I8, ,8I
-          "Yb,,8P _,88,_,dP   8I   8I   Yb, `YbadP'
-            "Y8P' 8P""Y88P'   8I   8I   `Y8888P"Y888
+  function removeBackgroundColor (el) {
+    var
+      prefix = 'color-bg-',
+      classes;
 
- */
-  console.log('alive');
+    el = (el.jquery) ? el[0] : el;
+
+    classes = el.className.split(' ').filter(function(c) {
+        return c.lastIndexOf(prefix, 0) !== 0;
+    });
+
+    el.className = classes.join(' ');
+  }
+
+  function addBackgroundColor (el, color) {
+    var colorClass;
+    el = (el.jquery) ? el[0] : el;
+    colorClass = 'color-bg-' + color;
+    el.className += colorClass;
+  }
+
+  var
+    preferences = $('#preferences');
+
+  preferences.on('click', '.gear', function gearOnClick () {
+    preferences.toggleClass('show');
+  });
+
+  preferences.on('click', '.color', function colorOnClick () {
+    var color = $(this);
+    removeBackgroundColor(document.body);
+    addBackgroundColor(document.body, color.data('colorSetting'));
+  });
 });
--- rebrand/app/config.js
+++ rebrand/app/config.js
@@ -8,7 +8,11 @@ requirejs.config({
   },
   paths: {
     src: '../js',
-    requirejs: '../bower_components/requirejs/require'
+    requirejs: '../bower_components/requirejs/require',
+    bean: '../bower_components/bean/bean',
+    bonzo: '../bower_components/bonzo/bonzo',
+    qwery: '../bower_components/qwery/qwery',
+    jquery: '../bower_components/jquery/dist/jquery'
   },
   packages: [
 
